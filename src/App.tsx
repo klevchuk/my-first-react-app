46659c766e8cfec464004aa6fe612d302a54fe12
klevchuk <lemoniklava@gmail.com>
2023-09-08 18:01:43 +0800

    Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,62 @@ import './App.css';
 function App() {
     return (
         <div>
-            <h3>This is App component</h3>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
         </div>
     );
 }
 
 export default App;
 
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+
+    )
+}
+
 function Star() {
     return (
         <div>star</div>
+    )
+}
+
+function AppTitle() {
+    return <>This is App component</>
+}
+
+function Accordion() {
+    return (
+        <>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </>
+    )
+}
+
+function AccordionTitle() {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     )
 }
