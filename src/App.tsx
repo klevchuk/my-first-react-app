8650060aa4c60b0d23dc2d03a0fedb55ea3a52d4
klevchuk <lemoniklava@gmail.com>
2023-09-08 17:33:01 +0800

    component Star

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h3>This is App component</h3>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    );
 }
 
 export default App;
+
+function Star() {
+    return (
+        <div>star</div>
+    )
+}
